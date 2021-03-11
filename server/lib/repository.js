69ea17338eb8034b3b25e83b05af2606b1a9543f
Brennan Jackson <btj9560@rit.edu>
2021-03-10 22:09:49 -0500

    Added functionality to search document by term. Regex needs work still

--- server/lib/repository.js
+++ server/lib/repository.js
@@ -7,19 +7,58 @@
  * repository.js
  *  Data layer to interact with our mongo DB instance
  */
- const log4js = require('log4js') 
+ const log4js = require('log4js')
+ const MongoPool = require('./mongopool.js') 
 
  var Repository = function() {
 
     //initialize logger
     const logger = log4js.getLogger('Repository')
 
+    //initialize mongo connection
+    const mongo = new MongoPool()
+    /**
+     * I am not using a try/catch for the initial startup.
+     * If an error is thrown when the application starts because 
+     * we cannot connect to the databse we should let the app crash
+     * and restart it after investigating.
+     */
+    mongo.init()
+    logger.info("Connected to database")
+
     /**
      * @function searchDocumentsByTerm search the database for documents by search terms
      * @param {searchTerms} array of search terms to query the database for
      */
     function searchDocumentsByTerm(searchTerms) {
-
+        logger.info("Query made against serach terms: " + searchTerms)
+        return new Promise((resolve, reject) => {
+            try {
+                mongo.connect((db) => {
+                    let regex = new RegExp(searchTerms, "i") //create regex expression with search terms
+                    db.collection("Routes").find(
+                        {
+                            desc: regex
+                        },
+                        {
+                            _id: 0,
+                            loc: 0
+                        },
+                        (err, res) => {
+                            if(err) {
+                                reject(err)
+                            }
+                            logger.info("Query successful")
+                            resolve(res)
+                        }
+                    )
+                })
+            }
+            catch(err) {
+                logger.warn("Error thrown in searchDocumentsByTerm: " + err)
+                reject(err)
+            }
+        })
     }
 
     /**
